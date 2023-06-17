a3186ffe77bbf0446401f1bde62004852e83be6a
visulagee <135705307+visulagee@users.noreply.github.com>
2023-06-17 20:45:23 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,27 @@
 input.onButtonPressed(Button.A, function () {
-    I2C_LCD1602.LcdInit(0)
-    basic.showIcon(IconNames.Yes)
-    I2C_LCD1602.on()
-    I2C_LCD1602.BacklightOn()
-    I2C_LCD1602.ShowString("fan is on", 0, 0)
+    I2C_LCD1602.clear()
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.analogWritePin(AnalogPin.P1, 500)
+    basic.showLeds(`
+        . . . # .
+        . . # . #
+        . # . . .
+        # . . . .
+        . . . . .
+        `)
+    I2C_LCD1602.on()
+    I2C_LCD1602.BacklightOn()
+    I2C_LCD1602.ShowString("Fan is on", 0, 0)
 })
 input.onButtonPressed(Button.B, function () {
-    I2C_LCD1602.LcdInit(0)
+    I2C_LCD1602.clear()
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
     basic.showIcon(IconNames.No)
     I2C_LCD1602.on()
     I2C_LCD1602.BacklightOn()
-    I2C_LCD1602.ShowString("fan is off", 0, 0)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.analogWritePin(AnalogPin.P1, 0)
+    I2C_LCD1602.ShowString("Fan is off", 0, 0)
 })
+I2C_LCD1602.LcdInit(0)
+I2C_LCD1602.clear()
+I2C_LCD1602.ShowString("Fan is ready", 0, 0)
